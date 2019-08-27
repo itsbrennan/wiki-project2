44e05bda69debe17a9b1257363e9b9dfddd1c392
Brennan Martin <Brennan.Martin@arrow.com>
2019-08-27 10:28:20 -0600

    initial / final commit

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
+// variables
 const input = document.querySelector('.main-search');
-const searchBtn = document.querySelector('.main-btn')
+const searchBtn = document.querySelector('.main-btn');
 
+// events
 searchBtn.addEventListener('click', searchWiki);
 
+//functions
+function searchWiki(e) {
+  e.preventDefault();
+  showGif('show');
+  let searchValue = input.value;
 
+  const origin = 'https://en.wikipedia.org/';
+  const url = `${origin}w/api.php?action=query&format=json&origin=*&list=search&srsearch=${searchValue}`;
 
-function searchWiki(event) {
- event.preventDefault();
- showGifi('show')
- let searchValue = input.value;
- let origin = 'https://en.wikipedia.org'
- // /w/api.php?action=query&format=json&prop=&list=search&srsearch=apple
-
-
- let url = `${origin}/w/api.php?action=query&origin=*&format=json&list=search&srsearch=${searchValue}`;
-
- fetch(url).then(function (data) { return data.json() }).then(displayData);
-
-
+  fetch(url)
+    .then(data => {
+      return data.json();
+    })
+    .then(displayData)
+    .catch(error => console.log(error));
 }
 
-// showGifi
-function showGifi(value) {
- if (value === 'show') {
-  document.querySelector('.wait-icon').classList.add('show')
- }
- else if (value === 'hide') {
-  document.querySelector('.wait-icon').classList.remove('show')
- }
+// show / hide gif
+function showGif(value) {
+  if (value === 'show') {
+    document.querySelector('.wait-icon').classList.add('show');
+  } else if (value === 'hide') {
+    document.querySelector('.wait-icon').classList.remove('show');
+  } else {
+  }
 }
+
 // display data
 function displayData(data) {
- console.log(data);
+  showGif('hide');
+  let result = data.query.search;
+  let output = '';
 
- showGifi('hide')
- let output = '';
- let result = data.query.search;
- result.forEach(function (item) {
-  output += `<li class="search-item">
-    <h2 class="search-item__title">${item.title}</h2>
-    <p class="search-item__text">${item.snippet}</p>
-    <a href="http://en.wikipedia.org/?curid=${ item.pageid}" target="_blank" class="search-item__link">Read More...</a>
-   </li>`
- })
- document.querySelector('.results').innerHTML = output;
+  result.forEach(item => {
+    output += `   
+    <li class="search-item">
+      <h2 class="search-item__title">${item.title}</h2>
+      <p class="search-item__text">${item.snippet}</p>
+      <a href="http://en.wikipedia.org/?curid=${item.pageid}" class="search-item__link" target="_blank">read more ...</a>
+    </li>`;
+  });
+  document.querySelector('.results').innerHTML = output;
 }
